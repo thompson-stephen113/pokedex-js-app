301bfb8254b31b720d4518807f33aceccd271e91
thompson-stephen113 <thompson.stephen113@gmail.com>
2023-07-07 08:55:31 -0400

    created an initial variable containing the array of Pokemon
    The array consists of 3 Pokemon objects and their key values. The console.log function is to test the functionality of the array.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+let pokemonList = [
+    {
+        name: "Bulbasaur", 
+        height: 0.7, 
+        type: [
+            "grass", 
+            "poison"
+        ]
+    },
+    {
+        name: "Ivysaur", 
+        height: 1.0, 
+        type: [
+            "grass", 
+            "poison"
+        ]
+    },
+    {
+        name: "Venusaur",
+        height: 2.0,
+        type: [
+            "grass", 
+            "poison"
+        ]
+    }
+];
+
+console.log(pokemonList[0].name)
