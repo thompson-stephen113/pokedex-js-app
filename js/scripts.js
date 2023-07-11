3106567829866611dddad94d5c3a54e989933973
thompson-stephen113 <thompson.stephen113@gmail.com>
2023-07-11 09:13:28 -0400

    expanded the list of Pokemon objects in pokemonList and added a for loop
    Added Charmander and Squirtle along with their evolutions and a for loop to write their names to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,51 @@ let pokemonList = [
     {
         name: "Bulbasaur", 
         height: 0.7, 
-        type: [
-            "grass", 
-            "poison"
-        ]
+        type: ["grass", "poison"]
     },
     {
         name: "Ivysaur", 
         height: 1.0, 
-        type: [
-            "grass", 
-            "poison"
-        ]
+        type: ["grass", "poison"]
     },
     {
         name: "Venusaur",
         height: 2.0,
-        type: [
-            "grass", 
-            "poison"
-        ]
+        type: ["grass", "poison"]
+    },
+    {
+        name: "Charmander",
+        height: 0.6,
+        type: ["fire"]
+    },
+    {
+        name: "Charmeleon",
+        height: 1.1,
+        type: ["fire"]
+    },
+    {
+        name: "Charizard",
+        height: 1.7,
+        type: ["fire", "flying"]
+    },
+    {
+        name: "Squirtle",
+        height: 0.5,
+        type: ["water"]
+    },
+    {
+        name: "Wartortle",
+        height: 1.0,
+        type: ["water"]
+    },
+    {
+        name: "Blastoise",
+        height: 1.6,
+        type: ["water"]
     }
 ];
 
-console.log(pokemonList[0].name)
+for (i = 0; i <= pokemonList.length; i++) {
+    document.write(pokemonList[i].name);
+}
+
