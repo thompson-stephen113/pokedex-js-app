c4909122a105933e7d1cf8b04b699b4f630c7eed
thompson-stephen113 <thompson.stephen113@gmail.com>
2023-07-20 14:27:38 -0400

    stored the main array in an IIFE and changed the for loop to a forEach loop
    Created pokemonRepository and stored pokemonList in it as an IIFE, including getAll and add functions.
    Changed the for loop to a forEach loop, and updated empty string variable to be more descriptive.
    Updated comments to be more cohesive and appropriately placed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,86 @@
-(function() {    
-    // List of Pokemon and some general stats
+// IIFE holds the pokemonList array, stored in pokemonRepository
+let pokemonRepository = (function() {
+    // Array of Pokemon objects and some general stat properties
     let pokemonList = [
-        {
-            name: "Bulbasaur", 
-            height: 0.7, 
-            type: ["grass", "poison"]
-        },
-        {
-            name: "Ivysaur", 
-            height: 1.0, 
-            type: ["grass", "poison"]
-        },
-        {
-            name: "Venusaur",
-            height: 2.0,
-            type: ["grass", "poison"]
-        },
-        {
-            name: "Charmander",
-            height: 0.6,
-            type: ["fire"]
-        },
-        {
-            name: "Charmeleon",
-            height: 1.1,
-            type: ["fire"]
-        },
-        {
-            name: "Charizard",
-            height: 1.7,
-            type: ["fire", "flying"]
-        },
-        {
-            name: "Squirtle",
-            height: 0.5,
-            type: ["water"]
-        },
-        {
-            name: "Wartortle",
-            height: 1.0,
-            type: ["water"]
-        },
-        {
-            name: "Blastoise",
-            height: 1.6,
-            type: ["water"]
-        }
+    { 
+        name: "Bulbasaur", 
+        height: 0.7, 
+        type: ["Grass", "Poison"]
+    },
+    {
+        name: "Ivysaur", 
+        height: 1.0, 
+        type: ["Grass", "Poison"]
+    },
+    {
+        name: "Venusaur",
+        height: 2.0,
+        type: ["Grass", "Poison"]
+    },
+    {
+        name: "Charmander",
+        height: 0.6,
+        type: ["Fire"]
+    },
+    {
+        name: "Charmeleon",
+        height: 1.1,
+        type: ["Fire"]
+    },
+    {
+        name: "Charizard",
+        height: 1.7,
+        type: ["Fire", "Flying"]
+    },
+    {
+        name: "Squirtle",
+        height: 0.5,
+        type: ["Water"]
+    },
+    {
+        name: "Wartortle",
+        height: 1.0,
+        type: ["Water"]
+    },
+    {
+        name: "Blastoise",
+        height: 1.6,
+        type: ["Water"]
+    }
     ];
 
-    let processedList = " ";
+    function getAll() {
+        return pokemonList;
+    }
 
-    // for loop to iterate over each Pokemon object in the pokemonList array
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
-    pokemonList.forEach(listIteration);
-
-    function listIteration(pokemon) {
-        if (pokemon.height < 1) {
-            processedList += "<p>" + pokemon.name + " (height: " + pokemon.height + ")" + " - It's tiny!" + "</p>";
-        } else if (pokemon.height >= 1 && pokemon.height <= 1.5) {
-            processedList += "<p>" + pokemon.name + " (height: " + pokemon.height + ")" + "</p>";
-        } else {
-            processedList += "<p>" + pokemon.name + " (height: " + pokemon.height + ")" + " - It's huge!" + "</p>";
-        }
+    return {
+        getAll: getAll,
+        add: add
     };
+})();
 
-    // loop will document each Pokemon and their heights, highlighting ones that are above or below average
+// Storing an empty string in a variable to be populated by forEach loop and written to webpage
+let pokedexEntry = " ";
 
-    document.getElementById("container").innerHTML = processedList;
-})()
+// Creates a function to run through if-else statements
+function listIteration(pokemon) {
+    // forEach loop iterates through pokemonList and documents each Pokemon and their heights, highlighting ones that are above or below average
+    if (pokemon.height < 1) {
+        pokedexEntry += "<p>" + pokemon.name + " (height: " + pokemon.height + ")" + " - It's tiny!" + "</p>";
+    } else if (pokemon.height >= 1 && pokemon.height <= 1.5) {
+        pokedexEntry += "<p>" + pokemon.name + " (height: " + pokemon.height + ")" + "</p>";
+    } else {
+        pokedexEntry += "<p>" + pokemon.name + " (height: " + pokemon.height + ")" + " - It's huge!" + "</p>";
+    }
+};
+
+// listIteration function fetches all objects in the pokemonList through the pokemonRepository and executes the loop
+pokemonRepository.getAll().forEach(listIteration);
+
+
+// Link to CSS through HTML by fetching the "container" id from <main>
+document.getElementById("container").innerHTML = pokedexEntry;
